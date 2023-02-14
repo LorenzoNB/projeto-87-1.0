f609e4a8575e3ec6030738f70385b25896eca699
LorenzoNB <98979295+LorenzoNB@users.noreply.github.com>
2023-02-14 13:59:23 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+
+import HomeScreen from "./screens/Home";
+import StarMapScreen from "./screens/StarMap";
+import DailyPicScreen from "./screens/DailyPic";
+import SpaceCraftScreen from "./screens/SpaceCraft";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return(
+      <NavigationContainer>
+        <Stack.Navigator initialRoutName="Home" screenOptions={{
+          headerShown: false 
+        }}> 
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="StarMap" component={StarMapScreen}/>
+          <Stack.Screen name="DailyPic" component={DailyPicScreen}/>
+          <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen}/>
+
+            </Stack.Navigator>
+          </NavigationContainer>
+
+
+  
+  
+    );
+  }
+
+
+
+export  default App();
+ 
+
